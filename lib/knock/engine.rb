c471689e588333be75edd3b432ca1903414f94d2
Ng Tze Yang <ngty77@gmail.com>
2017-09-24 10:31:25 +0800

    SIGH, just so we can run test & dont get generator bomb in production.

--- lib/knock/engine.rb
+++ lib/knock/engine.rb
@@ -1,11 +1,17 @@
 module Knock
   class Engine < ::Rails::Engine
 
-    if ::Rails.const_defined? 'Generators'
+    def self.require_all!
       config.eager_load_paths += Dir["#{config.root}/lib/**/"]
-    else
-      Dir["#{config.root}/lib/knock/**/*.rb"].each{|f| require f }
     end
+
+    def self.require_default!
+      Dir["#{config.root}/lib/knock/**/*.rb"].
+        each{|f| require f unless f == __FILE__ }
+    end
+
+    %w(production staging).include?(::Rails.env) ?
+      require_default! : require_all!
 
     isolate_namespace Knock
   end
